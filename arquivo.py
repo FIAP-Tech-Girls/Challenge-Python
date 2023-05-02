3f571d7a81dd812e8386ff0161e70c2d62a7f782
letyresina <leticiaresina@gmail.com>
2023-05-02 09:46:00 -0300

    Feito a parte da entrevista para quem não dirige

--- arquivo.py
+++ arquivo.py
@@ -8,7 +8,7 @@ def menuOpcoes():
     print(f"\n 1 - Participar da entrevista sobre trânsito \n 2 - Reportar algum problema em alguma via \n 3 - Saber a situação de alguma via \n 4 - Rotas alternativas para um mesmo destino \n 5 - Ver rotas favoritas \n 6 - Encerrar a conversa \n")
     opcao = int(input("Por favor, informe uma opção: "))
     return opcao
-
+            
 while True:
     opcao = menuOpcoes()
     if opcao == 1:
@@ -23,7 +23,34 @@ while True:
             case 1:
                 print(f"Inserir aqui a entrevista caso a pessoa dirija")
             case 2:
-                print(f"Inserir aqui a entrevista caso a pessoa não dirija")
+
+                print("Qual meio de transporte você utiliza com mais frequência? Veja no menu de opções:")
+
+                print("\n 1 - Uber/Táxi \n 2 - Carona (amigos(as), marido/esposa, entre outros) \n 3 - Transporte Público \n")
+
+                opcaoTransporte = int(input("Digite aqui apenas uma opção: "))
+
+                print("Em qual período você costuma sair com mais frequência?")
+
+                print("\n 1 - Manhã \n 2 - Tarde \n 3 - Noite \n ")
+
+                periodoSair = int(input("Digite aqui apenas uma opção: "))
+
+                print("Nesse período, você costuma pegar muito trânsito?")
+
+                print("\n 1 - Sim \n 2 - Não \n")
+                
+                transitoPeriodo = int(input("Digite aqui apenas uma opção: "))
+
+                if transitoPeriodo == 2:
+                    print("Então, em qual dos períodos abaixo você costuma pegar muito trânsito?")
+
+                    print("\n 1 - Manhã \n 2 - Tarde \n 3 - Noite \n ")
+                
+                print("Como você costuma reagir ao trânsito?")
+
+                reacaoTransito = input("Sinta-se livre para escrever como se sente, num limite de até 1000 caracteres: ")
+
             case _:
                 print(f"Opção inserida inválida. Por favor, reinicie e tente novamente!")
 
