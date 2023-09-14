14a8cd14208b7732456136f86da3beaa8ee3187f
letyresina <leticiaresina@gmail.com>
2023-09-14 14:06:04 -0300

    Subindo alteração no readme e pequena alteração no código

---
+++ arquivo.py
@@ -0,0 +1,11 @@
+# Criações de variáveis necessárias para início do programa.
+
+
+
+# Imports necessários para a aplicação
+
+import time # intervalo de tempo para o usuário poder visualizar e processar as informações
+import getpass # simular a entrada de senha de uma maneira segura (neste primeiro momento)
+
+# Funções
+
