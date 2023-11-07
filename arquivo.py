0e692c3d0e48c061e230bda53a2df9972b11b00a
letyresina <leticiaresina@gmail.com>
2023-11-06 22:42:19 -0300

    subindo função de menu

--- arquivo.py
+++ arquivo.py
@@ -8,3 +8,23 @@ import folium # para mostrar um mapa e não somente derivadas pro usuário
 import webbrowser # abrir o mapa automaticamente para o usuário, mesmmo que nesse momento seja somente com HTML
 import json # para abrir arquivos json externos da aplicação
 
+# Funções
+
+def menuOpcoes():
+    '''
+        Função utilizada para facilitar e deixar o código inteiro do sistema mais limpo para chamar 
+        o menu de opções, que se repete em todo looping. Há uma validação para evitar que erros aconteçam.
+    '''
+
+    print(f"\n 1 - Participar da entrevista de qualificação do trânsito; \n 2 - Ver situação de determinada rota; \n 3 - Feedback sobre determinada rota; \n 4 - Rotas alternativas para um destino; \n 5 - Favoritar uma rota \n 6 - Visualizar rotas favoritas \n 7 - Feedback sobre aplicativo; \n 8 - Encerrar Tiana.")
+    try:
+        opcao = int(input("Informe a opção desejada: "))
+        if (opcao < 1) or (opcao > 8):
+            raise TypeError
+        return opcao
+    except ValueError:
+        print("Por favor, informe somente números dentre as opções disponíveis!")
+        time.sleep(1)
+    except TypeError:
+        print("Por favor, digite uma opção válida para prosseguir.")
+        time.sleep(1)
