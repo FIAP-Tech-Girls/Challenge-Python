7f6d5374adfac5db42c5884a1effdab76b6f2ded
letyresina <leticiaresina@gmail.com>
2023-11-11 13:46:04 -0300

    ajustes nos testes do estado do trafego

--- testes/testeLogicaVerificaTransito2.py
+++ testes/testeLogicaVerificaTransito2.py
@@ -4,12 +4,17 @@ import time
 
 def contagemAleatoriaCarros():
     """
-        Função para gerar uma contagem aleatória de carros
+        Função para gerar uma contagem aleatória de carros, uma vez que não iríamos conseguir deixar
+        o ESP32 ligado 24 horas para nível de teste no programa em Python, então fizemos de uma forma 
+        aleatória que atualiza a cada 1 segundo.
     """
     return random.randint(0, 100)  # Ajuste o intervalo conforme necessário
 
-# Função para determinar o estado do tráfego com base na contagem de carros
+
 def estadoTrafego(carros):
+    """
+        Função para determinar o estado do tráfego com base na contagem de carros
+    """
     if 0 <= carros <= 20:
         return "Leve"
     elif 21 <= carros <= 50:
@@ -17,15 +22,20 @@ def estadoTrafego(carros):
     else:
         return "Intenso"
 
-# Função para gerar um arquivo JSON simulando a contagem de carros
+
 def arquivoJSON():
+    """
+        Função para gerar um arquivo JSON simulando a contagem de carros. Esse arquivo JSON é atualizado
+        a cada vez que o usuário pede para visualizar qual o estado do trafégo mostrando o último antes de
+        ser atualizado.
+    """
     carros = contagemAleatoriaCarros()
-    estado_trafego = estadoTrafego(carros)
+    estadoTrafegoAtual = estadoTrafego(carros)
 
     data = {
         "horário": time.strftime("%Y-%m-%d %H:%M:%S"),
         "carros": carros,
-        "estadoTrafego": estado_trafego,
+        "estadoTrafego": estadoTrafegoAtual,
         "local": "São Paulo"
     }
 
@@ -48,8 +58,8 @@ while True:
     opcao = input("Escolha uma opção: ")
 
     if opcao == "1":
-        estado_atual = obterEstadoTragego()
-        print(f"O estado atual do tráfego é: {estado_atual}")
+        estadoAtual = obterEstadoTragego()
+        print(f"O estado atual do tráfego é: {estadoAtual}")
     elif opcao == "2":
         print("Encerrando o programa.")
         break
